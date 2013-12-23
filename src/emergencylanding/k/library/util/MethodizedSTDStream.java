d42a7d92efa2367c240c84fe64966777bc970d2c
Kenzie Togami <ket1999@gmail.com>
2013-12-23 10:34:28 -0800

    Add more tests to ensure MSTD works.
    Also changed how MSTDS work.

--- src/emergencylanding/k/library/util/MethodizedSTDStream.java
+++ src/emergencylanding/k/library/util/MethodizedSTDStream.java
@@ -1,33 +1,97 @@
 package emergencylanding.k.library.util;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 
+import k.core.reflect.Reflect;
+
 public class MethodizedSTDStream extends ByteArrayOutputStream {
     PrintStream orig = null;
-    private boolean nextWriteIsOff;
-    static String sep = System.getProperty("line.seperator", "\n");
+    String data = "";
+    boolean lastNewline = true, autoFlush = false;
+    static OutputStream output = null;
+    static PrintStream outputps = null;
+    static {
+	try {
+	    output = new FileOutputStream("mstds.txt");
+	    outputps = new PrintStream(output, true);
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	}
+    }
 
     public MethodizedSTDStream(PrintStream out) {
 	orig = out;
+	try {
+	    autoFlush = Reflect.getField(Boolean.class, "autoFlush", out);
+	    outputps.println(autoFlush);
+	} catch (NoSuchFieldException e) {
+	    e.printStackTrace();
+	} catch (SecurityException e) {
+	    e.printStackTrace();
+	} catch (IllegalArgumentException e) {
+	    e.printStackTrace();
+	} catch (IllegalAccessException e) {
+	    e.printStackTrace();
+	}
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * Me figuring out how to do this.
+     * 
+     * write() is a actually a string, so convert the proper bytes to a string.
+     * Take that string, and if this is the first string or the last message was
+     * a new line, put the method descriptor data in front.
+     * 
+     * Other things to consider: Newlines at end of string, eg. "newline here\n"
+     * Newlines in string, eg. "newline here\nbut there is more"
+     * 
+     * Possibly do a recursive method with newlines, eg.
+     * "newline here\nwith more" -> "newline here", "\n", "with more"
+     */
     @Override
     public synchronized void write(byte[] b, int off, int len) {
-	byte[] aba = Arrays.copyOfRange(b, off, len);
-	if (new String(aba).endsWith(sep)) {
-	    orig.println();
-	    return;
+	try {
+	    String str = new String(Arrays.copyOfRange(b, off, len));
+	    str = replaceAllButLast(str, "\\r?\\n", "$0" + getMethod());
+	    if (lastNewline) {
+		data += getMethod() + str;
+	    } else {
+		data += str;
+	    }
+	    lastNewline = str.endsWith("\n");
+	    if (autoFlush) {
+		flush();
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
+    }
+
+    private String replaceAllButLast(String str, String check, String replace) {
+	String out = "";
+	String sub = str.replaceAll(check + "$", "");
+	out = str.replace(sub, sub.replaceAll(check, replace));
+	return out;
+    }
+
+    private String getMethod() {
+
 	StackTraceElement[] ste = new Throwable().getStackTrace();
 
-	// use 1 to skip over our entry, and also keep var after loop
-	int i = 1;
-	StackTraceElement s = null, sm1;
+	int i = StackTraceInfo.DUAL_INVOKING_METHOD_ZERO;
+	StackTraceElement s = null;
 	for (; i < ste.length; i++) {
 	    s = ste[i];
+	    // skip LUtils.print() because we want the method that called that
+	    // one.
 	    if (!s.getClassName().matches("^(java|sun)(.+?)")
 		    && !(s.getClassName().equals(LUtils.class.getName()) && s
 			    .getMethodName().equals("print"))) {
@@ -38,29 +102,17 @@ public class MethodizedSTDStream extends ByteArrayOutputStream {
 	    // there is no stack!
 	    throw new IllegalStateException("No stack!");
 	}
-	sm1 = ste[i - 1];
-	if (sm1.getClassName().equals(PrintStream.class.getName())
-		&& sm1.getMethodName().equals("print")) {
-	    nextWriteIsOff = true;
-	} else if (nextWriteIsOff) {
-	    orig.write(b, off, len);
-	    nextWriteIsOff = false;
-	    return;
-	}
 	String[] classsplit = s.getClassName().split("\\.");
-
-	String method = "[" + classsplit[classsplit.length - 1] + "."
+	return "[" + classsplit[classsplit.length - 1] + "."
 		+ s.getMethodName() + "()@" + Thread.currentThread().getName()
 		+ "] ";
-
-	orig.write((method + new String(b)).getBytes(), off,
-		len + method.getBytes().length);
     }
 
     @Override
     public void flush() throws IOException {
+	orig.write(data.getBytes());
 	orig.flush();
-	reset();
+	data = "";
     }
 
 }
