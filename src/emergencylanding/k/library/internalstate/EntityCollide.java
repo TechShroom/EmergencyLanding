19bed76700cdbab6f03e52c3a3627785f5eb3ade
Kenzie Togami <ket1999@gmail.com>
2014-01-09 08:51:56 +1100

    Get ready for collisions!

--- src/emergencylanding/k/library/internalstate/EntityCollide.java
+++ src/emergencylanding/k/library/internalstate/EntityCollide.java
@@ -5,22 +5,34 @@ import emergencylanding.k.library.lwjgl.tex.ELTexture;
 
 public class EntityCollide extends GravEntity {
 
+    double xLen, yLen;
+
     public EntityCollide(World w, float posX, float posY, float posZ,
             ELTexture texture) {
         super(w, posX, posY, posZ, texture);
+        xLen = texture.getWidth();
+        yLen = texture.getHeight();
     }
 
     public EntityCollide(World w, float posX, float posY, float posZ,
             ELTexture texture, float gravity) {
         super(w, posX, posY, posZ, texture, gravity);
+        xLen = texture.getWidth();
+        yLen = texture.getHeight();
     }
 
     @Override
     public void updateOnTick(float delta) {
         super.updateOnTick(delta);
         for (ELEntity other : w.getEntityList()) {
-
+            if (other instanceof EntityCollide) {
+                testCollide((EntityCollide) other);
+            }
         }
+    }
+
+    private boolean testCollide(EntityCollide other) {
+        return false;
     }
 
 }
