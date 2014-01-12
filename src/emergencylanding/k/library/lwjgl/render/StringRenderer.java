097a767f3f4e792b5211e72df584fbd536f51eef
Kenzie Togami <ket1999@gmail.com>
2014-01-12 11:03:59 -0800

    Sort-of fix StringRender
    Draws wrong, but at least it draws.

--- src/emergencylanding/k/library/lwjgl/render/StringRenderer.java
+++ src/emergencylanding/k/library/lwjgl/render/StringRenderer.java
@@ -7,19 +7,11 @@ import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
-import java.awt.image.DataBufferByte;
-import java.awt.image.DataBufferInt;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.IntBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.util.glu.GLU;
-
+import k.core.util.arrays.ResizableArray;
+import emergencylanding.k.library.lwjgl.Shapes;
 import emergencylanding.k.library.lwjgl.tex.BufferedTexture;
 import emergencylanding.k.library.lwjgl.tex.ELTexture;
 import emergencylanding.k.library.util.LUtils;
@@ -40,28 +32,18 @@ import emergencylanding.k.library.util.LUtils;
 public class StringRenderer {
     public final static int ALIGN_LEFT = 0, ALIGN_RIGHT = 1, ALIGN_CENTER = 2;
     /** Array that holds necessary information about the font characters */
-    private IntObject[] charArray = new IntObject[256];
+    private ELTexture[] charArray = new ELTexture[256];
 
     /** Map of user defined font characters (Character <-> IntObject) */
-    private Map<Character, IntObject> customChars = new HashMap<Character, IntObject>();
+    private Map<Character, ELTexture> customChars = new HashMap<Character, ELTexture>();
 
     /** Boolean flag on whether AntiAliasing is enabled or not */
     private boolean antiAlias;
 
     /** Font's size */
     private int fontSize = 0;
-
-    /** Font's height */
-    private int fontHeight = 0;
-
-    /** Texture used to cache the font 0-255 characters */
-    private int fontTextureID;
-
-    /** Default font texture width */
-    private int textureWidth = 512;
-
-    /** Default font texture height */
-    private int textureHeight = 512;
+    /** Height */
+    private int fontHeight;
 
     /** A reference to Java's AWT Font that we create our font texture from */
     private Font font;
@@ -71,30 +53,12 @@ public class StringRenderer {
 
     private int correctL = 9, correctR = 8;
 
-    private class IntObject {
-        /** Character's width */
-        public int width;
-
-        /** Character's height */
-        public int height;
-
-        /** Character's stored x position */
-        public int storedX;
-
-        /** Character's stored y position */
-        public int storedY;
-    }
-
     public StringRenderer(Font font, boolean antiAlias, char[] additionalChars) {
         this.font = font;
         this.fontSize = font.getSize() + 3;
         this.antiAlias = antiAlias;
 
         createSet(additionalChars);
-
-        fontHeight -= 1;
-        if (fontHeight <= 0)
-            fontHeight = 1;
     }
 
     public StringRenderer(Font font, boolean antiAlias) {
@@ -154,29 +118,7 @@ public class StringRenderer {
     }
 
     private void createSet(char[] customCharsArray) {
-        // If there are custom chars then I expand the font texture twice
-        if (customCharsArray != null && customCharsArray.length > 0) {
-            textureWidth *= 2;
-        }
-
-        // In any case this should be done in other way. Texture with size
-        // 512x512
-        // can maintain only 256 characters with resolution of 32x32. The
-        // texture
-        // size should be calculated dynamicaly by looking at character sizes.
-
         try {
-
-            BufferedImage imgTemp = new BufferedImage(textureWidth,
-                    textureHeight, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g = (Graphics2D) imgTemp.getGraphics();
-
-            g.setColor(new Color(0, 0, 0, 1));
-            g.fillRect(0, 0, textureWidth, textureHeight);
-
-            int rowHeight = 0;
-            int positionX = 0;
-            int positionY = 0;
 
             int customCharsLength = (customCharsArray != null) ? customCharsArray.length
                     : 0;
@@ -186,47 +128,21 @@ public class StringRenderer {
                 // get 0-255 characters and then custom characters
                 char ch = (i < 256) ? (char) i : customCharsArray[i - 256];
 
+                // create image
                 BufferedImage fontImage = getFontImage(ch);
 
-                IntObject newIntObject = new IntObject();
+                // inc height if needed
+                fontHeight = Math.max(fontHeight, fontImage.getHeight());
 
-                newIntObject.width = fontImage.getWidth();
-                newIntObject.height = fontImage.getHeight();
-
-                if (positionX + newIntObject.width >= textureWidth) {
-                    positionX = 0;
-                    positionY += rowHeight;
-                    rowHeight = 0;
+                if (i < 256) {
+                    // standard characters
+                    charArray[i] = new BufferedTexture(fontImage);
+                } else {
+                    // custom characters
+                    customChars.put(new Character(ch), new BufferedTexture(
+                            fontImage));
                 }
-
-                newIntObject.storedX = positionX;
-                newIntObject.storedY = positionY;
-
-                if (newIntObject.height > fontHeight) {
-                    fontHeight = newIntObject.height;
-                }
-
-                if (newIntObject.height > rowHeight) {
-                    rowHeight = newIntObject.height;
-                }
-
-                // Draw it here
-                g.drawImage(fontImage, positionX, positionY, null);
-
-                positionX += newIntObject.width;
-
-                if (i < 256) { // standard characters
-                    charArray[i] = newIntObject;
-                } else { // custom characters
-                    customChars.put(new Character(ch), newIntObject);
-                }
-
-                fontImage = null;
             }
-
-            fontTextureID = loadImage(imgTemp);
-
-            // .getTexture(font.toString(), imgTemp);
 
         } catch (Exception e) {
             LUtils.print("Failed to create font.");
@@ -235,42 +151,22 @@ public class StringRenderer {
     }
 
     private void drawQuad(float drawX, float drawY, float drawX2, float drawY2,
-            float srcX, float srcY, float srcX2, float srcY2) {
-        float DrawWidth = drawX2 - drawX;
-        float DrawHeight = drawY2 - drawY;
-        float TextureSrcX = srcX / textureWidth;
-        float TextureSrcY = srcY / textureHeight;
-        float SrcWidth = srcX2 - srcX;
-        float SrcHeight = srcY2 - srcY;
-        float RenderWidth = (SrcWidth / textureWidth);
-        float RenderHeight = (SrcHeight / textureHeight);
-
-        // GL11.glTexCoord2f(TextureSrcX, TextureSrcY);
-        // GL11.glVertex2f(drawX, drawY);
-        // GL11.glTexCoord2f(TextureSrcX, TextureSrcY + RenderHeight);
-        // GL11.glVertex2f(drawX, drawY + DrawHeight);
-        // GL11.glTexCoord2f(TextureSrcX + RenderWidth, TextureSrcY +
-        // RenderHeight);
-        // GL11.glVertex2f(drawX + DrawWidth, drawY + DrawHeight);
-        // GL11.glTexCoord2f(TextureSrcX + RenderWidth, TextureSrcY);
-        // GL11.glVertex2f(drawX + DrawWidth, drawY);
+            ELTexture tex) {
+        Shapes.getQuad(new VertexData().setXYZ(drawX2, drawY2, 0),
+                new VertexData().setXYZ(drawX, drawY, 0), Shapes.XY)
+                .setTexture(tex).draw().destroy();
     }
 
     public int getWidth(String whatchars) {
         int totalwidth = 0;
-        IntObject intObject = null;
         int currentChar = 0;
         for (int i = 0; i < whatchars.length(); i++) {
             currentChar = whatchars.charAt(i);
-            if (currentChar < 256) {
-                intObject = charArray[currentChar];
-            } else {
-                intObject = (IntObject) customChars.get(new Character(
-                        (char) currentChar));
-            }
+            ELTexture tex = (currentChar < 256) ? charArray[currentChar]
+                    : customChars.get(new Character((char) currentChar));
 
-            if (intObject != null)
-                totalwidth += intObject.width;
+            if (tex != null)
+                totalwidth += tex.dim.width;
         }
         return totalwidth;
     }
@@ -302,7 +198,7 @@ public class StringRenderer {
     public void drawString(float x, float y, String whatchars, int startIndex,
             int endIndex, float scaleX, float scaleY, int format) {
 
-        IntObject intObject = null;
+        ELTexture tex = null;
         int charCurrent;
 
         int totalwidth = 0;
@@ -327,12 +223,11 @@ public class StringRenderer {
                 if (charCurrent == '\n')
                     break;
                 if (charCurrent < 256) {
-                    intObject = charArray[charCurrent];
+                    tex = charArray[charCurrent];
                 } else {
-                    intObject = (IntObject) customChars.get(new Character(
-                            (char) charCurrent));
+                    tex = customChars.get(new Character((char) charCurrent));
                 }
-                totalwidth += intObject.width - correctL;
+                totalwidth += tex.dim.width - correctL;
             }
             totalwidth /= -2;
         }
@@ -345,22 +240,18 @@ public class StringRenderer {
 
         }
 
-        // GL11.glBindTexture(GL11.GL_TEXTURE_2D, fontTextureID);
-        // GL11.glBegin(GL11.GL_QUADS);
-
         while (i >= startIndex && i <= endIndex) {
 
             charCurrent = whatchars.charAt(i);
             if (charCurrent < 256) {
-                intObject = charArray[charCurrent];
+                tex = charArray[charCurrent];
             } else {
-                intObject = (IntObject) customChars.get(new Character(
-                        (char) charCurrent));
+                tex = customChars.get(new Character((char) charCurrent));
             }
 
-            if (intObject != null) {
+            if (tex != null) {
                 if (d < 0)
-                    totalwidth += (intObject.width - c) * d;
+                    totalwidth += (tex.dim.width - c) * d;
                 if (charCurrent == '\n') {
                     startY -= fontHeight * d;
                     totalwidth = 0;
@@ -370,102 +261,27 @@ public class StringRenderer {
                             if (charCurrent == '\n')
                                 break;
                             if (charCurrent < 256) {
-                                intObject = charArray[charCurrent];
+                                tex = charArray[charCurrent];
                             } else {
-                                intObject = (IntObject) customChars
-                                        .get(new Character((char) charCurrent));
+                                tex = customChars.get(new Character(
+                                        (char) charCurrent));
                             }
-                            totalwidth += intObject.width - correctL;
+                            totalwidth += tex.dim.width - correctL;
                         }
                         totalwidth /= -2;
                     }
                     // if center get next lines total width/2;
                 } else {
-                    drawQuad((totalwidth + intObject.width) * scaleX + x,
-                            startY * scaleY + y, totalwidth * scaleX + x,
-                            (startY + intObject.height) * scaleY + y,
-                            intObject.storedX + intObject.width,
-                            intObject.storedY + intObject.height,
-                            intObject.storedX, intObject.storedY);
+                    drawQuad((totalwidth + tex.dim.width) * scaleX + x, startY
+                            * scaleY + y, totalwidth * scaleX + x,
+                            (startY + tex.dim.height) * scaleY + y, tex);
                     if (d > 0)
-                        totalwidth += (intObject.width - c) * d;
+                        totalwidth += (tex.dim.width - c) * d;
                 }
                 i += d;
 
             }
         }
-        // GL11.glEnd();
-    }
-
-    public static int loadImage(BufferedImage bi) {
-        ELTexture tex = new BufferedTexture(bi);
-        return tex.getID();
-    }
-
-    public static int loadImageOld(BufferedImage bufferedImage) {
-        try {
-            short width = (short) bufferedImage.getWidth();
-            short height = (short) bufferedImage.getHeight();
-            // textureLoader.bpp = bufferedImage.getColorModel().hasAlpha() ?
-            // (byte)32 : (byte)24;
-            int bpp = (byte) bufferedImage.getColorModel().getPixelSize();
-            ByteBuffer byteBuffer;
-            DataBuffer db = bufferedImage.getData().getDataBuffer();
-            if (db instanceof DataBufferInt) {
-                int intI[] = ((DataBufferInt) (bufferedImage.getData()
-                        .getDataBuffer())).getData();
-                byte newI[] = new byte[intI.length * 4];
-                for (int i = 0; i < intI.length; i++) {
-                    byte b[] = intToByteArray(intI[i]);
-                    int newIndex = i * 4;
-
-                    newI[newIndex] = -1;
-                    newI[newIndex + 1] = -1;
-                    newI[newIndex + 2] = -1;
-                    newI[newIndex + 3] = b[0];
-                }
-
-                byteBuffer = ByteBuffer
-                        .allocateDirect(width * height * (bpp / 8))
-                        .order(ByteOrder.nativeOrder()).put(newI);
-            } else {
-                byteBuffer = ByteBuffer
-                        .allocateDirect(width * height * (bpp / 8))
-                        .order(ByteOrder.nativeOrder())
-                        .put(((DataBufferByte) (bufferedImage.getData()
-                                .getDataBuffer())).getData());
-            }
-            byteBuffer.flip();
-
-            int internalFormat = GL11.GL_RGBA8, format = GL11.GL_RGBA;
-            IntBuffer textureId = BufferUtils.createIntBuffer(1);
-            ;
-            GL11.glGenTextures(textureId);
-            GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureId.get(0));
-
-            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S,
-                    GL11.GL_CLAMP);
-            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T,
-                    GL11.GL_CLAMP);
-
-            GL11.glTexParameteri(GL11.GL_TEXTURE_2D,
-                    GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-            GL11.glTexParameteri(GL11.GL_TEXTURE_2D,
-                    GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-
-            GL11.glTexEnvf(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE,
-                    GL11.GL_MODULATE);
-
-            GLU.gluBuild2DMipmaps(GL11.GL_TEXTURE_2D, internalFormat, width,
-                    height, format, GL11.GL_UNSIGNED_BYTE, byteBuffer);
-            return textureId.get(0);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
-
-        return -1;
     }
 
     public static boolean isSupported(String fontname) {
@@ -487,9 +303,13 @@ public class StringRenderer {
     }
 
     public void destroy() {
-        IntBuffer scratch = BufferUtils.createIntBuffer(1);
-        scratch.put(0, fontTextureID);
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
-        GL11.glDeleteTextures(scratch);
+        ELTexture[] all = new ELTexture[charArray.length + customChars.size()];
+        System.arraycopy(charArray, 0, all, 0, charArray.length);
+        char[] c = new ResizableArray<char[]>(char[].class,
+                customChars.values()).getArray();
+        System.arraycopy(c, 0, all, charArray.length, c.length);
+        for (ELTexture t : all) {
+            t.kill();
+        }
     }
 }
