ca8e36ae934e7b43c44a7310eee79c237f82203d
Kenzie Togami <ket1999@gmail.com>
2014-01-23 18:10:46 -0800

    Remove unneeded import

--- src/emergencylanding/k/library/lwjgl/render/GLData.java
+++ src/emergencylanding/k/library/lwjgl/render/GLData.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
 
