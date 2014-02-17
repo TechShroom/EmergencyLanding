afed7d904c7c028517e445934661ffbba3619e76
Kenzie Togami <ket1999@gmail.com>
2014-02-17 13:30:05 -0800

    Update doc

--- src/emergencylanding/k/exst/modules/IModule.java
+++ src/emergencylanding/k/exst/modules/IModule.java
@@ -1,14 +1,14 @@
 package emergencylanding.k.exst.modules;
 
+import emergencylanding.k.exst.mods.IMod;
+
 /**
- * A to be implemented module system for EL. Might mean that we'll split up EL
- * like we did KCore. Probably not, but it's a possibility.
+ * A to be implemented module system for EL. Used like the mods system, but is
+ * less of a public API than that. Things in the module system should not be
+ * relied upon.
  * 
  * @author Kenzie Togami
  */
-public abstract class IModule {
-
-    public IModule() {
-    }
+public abstract class IModule implements IMod {
 
 }
