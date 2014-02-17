602206dadad6423b3a6a5019bf144b95d72f5b9d
Kenzie Togami <ket1999@gmail.com>
2014-02-16 21:27:50 -0800

    Module system? Maybe.

---
+++ src/emergencylanding/k/exst/modules/IModule.java
@@ -0,0 +1,14 @@
+package emergencylanding.k.exst.modules;
+
+/**
+ * A to be implemented module system for EL. Might mean that we'll split up EL
+ * like we did KCore. Probably not, but it's a possibility.
+ * 
+ * @author Kenzie Togami
+ */
+public abstract class IModule {
+
+    public IModule() {
+    }
+
+}
