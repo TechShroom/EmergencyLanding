94a9bd762e338fe6ba2636615483156a341b20a2
Kenzie Togami <ket1999@gmail.com>
2014-02-28 21:22:58 -0800

    Don't rotate here

--- src/emergencylanding/k/library/lwjgl/render/TextureRender.java
+++ src/emergencylanding/k/library/lwjgl/render/TextureRender.java
@@ -2,21 +2,17 @@ package emergencylanding.k.library.lwjgl.render;
 
 import emergencylanding.k.library.internalstate.ELEntity;
 import emergencylanding.k.library.lwjgl.Shapes;
-import emergencylanding.k.library.util.DrawableUtils;
 
 public class TextureRender extends Render<ELEntity> {
 
     @Override
-    public void doRender(ELEntity entity, float posX, float posY,
-            float posZ) {
-        DrawableUtils.glBeginRot(-45, 0, 0, 1);
-        VBAO quad = Shapes.getQuad(new VertexData(),
-                new VertexData().setXYZ((float) entity.getTex().getWidth(),
-                        (float) entity.getTex().getHeight(), posZ), Shapes.XY);
+    public void doRender(ELEntity entity, float posX, float posY, float posZ) {
+        VBAO quad = Shapes.getQuad(new VertexData(), new VertexData().setXYZ(
+                (float) entity.getTex().getWidth(), (float) entity.getTex()
+                        .getHeight(), posZ), Shapes.XY);
         quad.setTexture(entity.getTex());
         quad.setXYZOff(entity.getInterpolated());
         quad.draw();
-        DrawableUtils.glEndRot();
     }
 
 }
