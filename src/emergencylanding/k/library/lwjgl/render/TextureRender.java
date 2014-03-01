7e7324052f6c1ceb75be132b07bad3009ffec6fe
Kenzie Togami <ket1999@gmail.com>
2014-02-28 21:42:01 -0800

    Use glTrans instead of setXYZOff

--- src/emergencylanding/k/library/lwjgl/render/TextureRender.java
+++ src/emergencylanding/k/library/lwjgl/render/TextureRender.java
@@ -2,17 +2,19 @@ package emergencylanding.k.library.lwjgl.render;
 
 import emergencylanding.k.library.internalstate.ELEntity;
 import emergencylanding.k.library.lwjgl.Shapes;
+import emergencylanding.k.library.util.DrawableUtils;
 
 public class TextureRender extends Render<ELEntity> {
 
     @Override
     public void doRender(ELEntity entity, float posX, float posY, float posZ) {
+        DrawableUtils.glBeginTrans(posX, posY, posZ);
         VBAO quad = Shapes.getQuad(new VertexData(), new VertexData().setXYZ(
                 (float) entity.getTex().getWidth(), (float) entity.getTex()
                         .getHeight(), posZ), Shapes.XY);
         quad.setTexture(entity.getTex());
-        quad.setXYZOff(entity.getInterpolated());
         quad.draw();
+        DrawableUtils.glEndTrans();
     }
 
 }
