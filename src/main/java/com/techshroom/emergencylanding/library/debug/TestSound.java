ec07888bae427ff49e183621020ddf6795605212
Kenzie Togami <ket1999@gmail.com>
2016-01-18 22:01:07 -0800

    Implement sound library with Java Sound.

--- src/main/java/com/techshroom/emergencylanding/library/debug/TestSound.java
+++ src/main/java/com/techshroom/emergencylanding/library/debug/TestSound.java
@@ -22,49 +22,44 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-//package com.techshroom.emergencylanding.library.debug;
-//
-//import java.util.HashMap;
-//
-//import org.lwjgl.opengl.Display;
-//
-//import com.techshroom.emergencylanding.library.internalstate.ELEntity;
-//import com.techshroom.emergencylanding.library.lwjgl.DisplayLayer;
-//import com.techshroom.emergencylanding.library.lwjgl.render.Render;
-//import com.techshroom.emergencylanding.library.main.KMain;
-//import com.techshroom.emergencylanding.library.sound.SoundPlayer;
-//import com.techshroom.emergencylanding.library.util.LUtils;
-//
-//public class TestSound extends KMain {
-//
-//    public static void main(String[] args) throws Exception {
-//        DisplayLayer.initDisplay(false, 800, 600, "Testing EL Sound", true,
-//                args);
-//        while (!Display.isCloseRequested()) {
-//            DisplayLayer.loop(120);
-//        }
-//        DisplayLayer.destroy();
-//        System.exit(0);
-//    }
-//
-//    @Override
-//    public void onDisplayUpdate(int delta) {
-//        DisplayLayer.readDevices();
-//    }
-//
-//    @Override
-//    public void init(DisplayLayer layer, String[] args) {
-//        SoundPlayer.playWAV(LUtils.getELTop() + "/wav/test.wav", 1.0f,
-//                .50f, true);
-//        SoundPlayer.playWAV(LUtils.getELTop() + "/wav/test.wav", 1.0f,
-//                .10f, true);
-//        SoundPlayer.playWAV(LUtils.getELTop() + "/wav/test.wav", 1.0f,
-//                2.15f, true);
-//    }
-//
-//    @Override
-//    public void registerRenders(
-//            HashMap<Class<? extends ELEntity>, Render<? extends ELEntity>> classToRender) {
-//
-//    }
-//}
+package com.techshroom.emergencylanding.library.debug;
+
+import com.techshroom.emergencylanding.library.lwjgl.DisplayLayer;
+import com.techshroom.emergencylanding.library.main.KMain;
+import com.techshroom.emergencylanding.library.sound.SoundPlayer;
+import com.techshroom.emergencylanding.library.util.LUtils;
+
+public class TestSound extends KMain {
+
+    private static DisplayLayer layer;
+
+    public static void main(String[] args) throws Exception {
+        try {
+            layer = DisplayLayer.initDisplay(0, 800, 600, "Testing EL Sound",
+                    true, args);
+            while (!layer.shouldClose()) {
+                layer.loop(120);
+            }
+        } finally {
+            if (layer != null) {
+                layer.destroy();
+            }
+            DisplayLayer.finalExitCall();
+        }
+    }
+
+    @Override
+    public void onDisplayUpdate(int delta) {
+    }
+
+    @Override
+    public void init(DisplayLayer layer, String[] args) {
+        SoundPlayer.playWAV(LUtils.getELTop() + "/wav/test.wav", 1.0f, .50f,
+                true);
+        SoundPlayer.playWAV(LUtils.getELTop() + "/wav/test.wav", 1.0f, .10f,
+                true);
+        SoundPlayer.playWAV(LUtils.getELTop() + "/wav/test.wav", 1.0f, 2.15f,
+                true);
+    }
+
+}
--- src/main/java/com/techshroom/emergencylanding/library/sound/SoundPlayer.java
+++ src/main/java/com/techshroom/emergencylanding/library/sound/SoundPlayer.java
@@ -24,12 +24,22 @@
  */
 package com.techshroom.emergencylanding.library.sound;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import org.lwjgl.openal.ALC;
+import org.lwjgl.openal.ALContext;
 
 public class SoundPlayer {
 
     static {
-        ALC.create();
+        ALContext.create();
         Runtime.getRuntime().addShutdownHook(new Thread() {
 
             @Override
@@ -40,58 +50,56 @@ public class SoundPlayer {
     }
 
     /* TODO implement the hell out of this */
-//    /**
-//     * Plays .wav file
-//     * 
-//     * @param soundFile
-//     */
-//    public static Audio playWAV(String soundFile) {
-//        return playWAV(soundFile, 1.0f);
-//    }
-//
-//    /**
-//     * Plays .wav file
-//     * 
-//     * @param soundFile
-//     * @param volume
-//     */
-//    public static Audio playWAV(String soundFile, float volume) {
-//        return playWAV(soundFile, volume, 1.0f);
-//    }
-//
-//    /**
-//     * Plays .wav file
-//     * 
-//     * @param soundFile
-//     * @param volume
-//     * @param pitch
-//     */
-//    public static Audio playWAV(String soundFile, float volume, float pitch) {
-//        return playWAV(soundFile, volume, pitch, false);
-//    }
-//
-//    /**
-//     * Plays .wav file
-//     * 
-//     * @param soundFile
-//     * @param volume
-//     * @param pitch
-//     * @param loop
-//     */
-//    public static Audio playWAV(String soundFile, float volume, float pitch,
-//            boolean loop) {
-//        Audio wavA = null;
-//        try {
-//            InputStream iStream = LUtils.getInputStream(soundFile);
-//            BufferedInputStream bStream = new BufferedInputStream(iStream);
-//            wavA = AudioLoader.getAudio("WAV", bStream);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        SoundStore.get().setSoundVolume(volume);
-//
-//        wavA.playAsSoundEffect(pitch, 1.0f, loop);
-//        return wavA;
-//    }
+    /**
+     * Plays .wav file
+     * 
+     * @param soundFile
+     */
+    public static void playWAV(String soundFile) {
+        playWAV(soundFile, 1.0f);
+    }
+
+    /**
+     * Plays .wav file
+     * 
+     * @param soundFile
+     * @param volume
+     */
+    public static void playWAV(String soundFile, float volume) {
+        playWAV(soundFile, volume, 1.0f);
+    }
+
+    /**
+     * Plays .wav file
+     * 
+     * @param soundFile
+     * @param volume
+     * @param pitch
+     */
+    public static void playWAV(String soundFile, float volume, float pitch) {
+        playWAV(soundFile, volume, pitch, false);
+    }
+
+    /**
+     * Plays .wav file
+     * 
+     * @param soundFile
+     * @param volume
+     * @param pitch
+     * @param loop
+     */
+    public static void playWAV(String soundFile, float volume, float pitch,
+            boolean loop) {
+        try (
+                AudioInputStream data = AudioSystem
+                        .getAudioInputStream(Paths.get(soundFile).toFile())) {
+            Clip clip = AudioSystem.getClip();
+            clip.open(data);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException
+                | LineUnavailableException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
 }
